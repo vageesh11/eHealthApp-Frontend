ca23a7db7f2c9255da3b584b96c78c627f342d28
Rahamath M <rahamathgousiya@gmail.com>
2025-10-14 09:26:57 +0530

    removed unused logics

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import { FaBars, FaBell, FaUserCircle } from "react-icons/fa";
-import { useDispatch } from "react-redux";
-import { RootState,AppDispatch } from "../store/store";
-import { increment,clear } from "../store/counterSlice";
-import { useSelector } from "react-redux";
+
 
 interface HeaderProps {
   toggleSidebar: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
-  const count = useSelector((state:RootState)=>state.counter.value);
-  const todayDate = useSelector((state:RootState)=>state.date.value)
-  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <header className="bg-[rgb(13,152,186)]">
-      <span>count:{count}</span>
-      <button onClick={()=>dispatch(increment())}>increment</button>
-      <button onClick={()=>dispatch(clear())}>clear</button>
-      <p>Today's Date:{todayDate}</p>
+
       <div className="px-6 py-4 flex justify-between items-center">
-        
+
         <div className="flex items-center space-x-4 text-white">
           <button
             onClick={toggleSidebar}
@@ -31,7 +23,7 @@ const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
           <h1 className="text-xl font-bold">eHealthApp</h1>
         </div>
 
-       
+
         <div className="flex items-center space-x-4 text-white text-xl">
           <div className="p-2 rounded-full cursor-pointer hover:bg-[rgb(0,109,111)]">
             <FaBell />
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { FaHome, FaUserAlt, FaCog, FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { RootState } from "../store/store";
-import { useSelector } from "react-redux";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -11,26 +9,24 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
   const [isHovered, setIsHovered] = useState(false);
   const expanded = isOpen || isHovered;
-  const count = useSelector((state:RootState)=>state.counter.value);
+
 
   return (
     <div
-      className={`bg-[rgb(13,152,186)] text-white transition-all duration-300 ${
-        expanded ? "w-64" : "w-20"
-      }`}
+      className={`bg-[rgb(13,152,186)] text-white transition-all duration-300 ${expanded ? "w-64" : "w-20"
+        }`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div>count:{count}</div>
+
       <nav className="mt-6">
         <ul>
           {/* Home */}
           <li className="flex items-center p-4 hover:bg-[rgb(0,109,111)] cursor-pointer">
             <FaHome size={24} />
             <span
-              className={`ml-4 transition-opacity duration-300 ${
-                expanded ? "opacity-100" : "opacity-0 hidden"
-              }`}
+              className={`ml-4 transition-opacity duration-300 ${expanded ? "opacity-100" : "opacity-0 hidden"
+                }`}
             >
               Home
             </span>
@@ -44,9 +40,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
             >
               <FaUserAlt size={24} />
               <span
-                className={`ml-4 transition-opacity duration-300 ${
-                  expanded ? "opacity-100" : "opacity-0 hidden"
-                }`}
+                className={`ml-4 transition-opacity duration-300 ${expanded ? "opacity-100" : "opacity-0 hidden"
+                  }`}
               >
                 Profile
               </span>
@@ -57,9 +52,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
           <li className="flex items-center p-4 hover:bg-[rgb(0,109,111)] cursor-pointer">
             <FaCog size={24} />
             <span
-              className={`ml-4 transition-opacity duration-300 ${
-                expanded ? "opacity-100" : "opacity-0 hidden"
-              }`}
+              className={`ml-4 transition-opacity duration-300 ${expanded ? "opacity-100" : "opacity-0 hidden"
+                }`}
             >
               Settings
             </span>
@@ -69,9 +63,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
           <li className="flex items-center p-4 hover:bg-[rgb(0,109,111)] cursor-pointer">
             <FaSignOutAlt size={24} />
             <span
-              className={`ml-4 transition-opacity duration-300 ${
-                expanded ? "opacity-100" : "opacity-0 hidden"
-              }`}
+              className={`ml-4 transition-opacity duration-300 ${expanded ? "opacity-100" : "opacity-0 hidden"
+                }`}
             >
               Logout
             </span>
